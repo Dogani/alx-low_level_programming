4442176a9134113a4f540c9d6d3e764d99c0a0de
“Dogani” <doganipeter@gmail.com>
2023-06-19 12:32:41 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print lower and upper alphabet on the same line
+ * Return: 0
+ */
+int
+main(void)
+{
+	char c;
+	char C;
+
+	c = 'a';
+	C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+
+	return (0);
+}
