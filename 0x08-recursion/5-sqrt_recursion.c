919812389df34f99637d611ec4df8d14fbd8d749
“Dogani” <doganipeter@gmail.com>
2023-07-18 05:21:39 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,34 @@
 #include "main.h"
+
+int actual_sqrt_recursion(int n, int i);
+
 /**
- * _sqrt_recursion - returns the sqrt of a number
- * @n: first parameter
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
  *
- * Description: uses recursion
- * Return: Always(0) Success
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
-return (sqrt_value(1, n));
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
 }
 
 /**
- * sqrt_value - recursion function to find square root
- * @a: integer to be squared
- * @b: integer that is square of @a
- * Return: Integer
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
  */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
 
-int sqrt_value(int a, int b)
-{
-if (a * a > b)
-{
-return (-1);
-}
-if (a * a == b)
-{
-return (a);
-}
-else
-return (sqrt_value((a + 1), b));
-}
