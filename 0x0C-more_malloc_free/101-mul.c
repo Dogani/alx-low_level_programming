260e38e82bb0dcf3f57e865dde6974e520208f46
“Dogani” <doganipeter@gmail.com>
2023-07-17 23:07:58 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,8 +6,7 @@
  * @argc: number of arguments
  * @argv: arguments
  * Return: Nothing
- */
- 
+*/
 int main(int argc, char **argv)
 {
 int num1, num2, result;
@@ -21,7 +20,7 @@ else
 {
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-result = num1 * num2;
+result = num1 *num2;
 printf("%d\n", result);
 return (0);
 }
